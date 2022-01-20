e139bb260631f20464e8ac86d59d8a47e0f267c0
Seven Chor <hjlcarl@163.com>
2022-01-20 18:05:16 +0800

    修复bug及适应新版本dom

---
+++ theme.js
@@ -0,0 +1,13 @@
+// window.onload = function () {
+//   let LIs = document.getElementsByClassName("li");
+
+//   for (let i = 0; i < LIs.length; i++) {
+//     const li = LIs[i];
+//     for (let j = 0; j < li.children.length; j++) {
+//       const liChild = li.children[j];
+//       if (liChild.className == "list") {
+//         li.classList.add("hasChild");
+//       }
+//     }
+//   }
+// };

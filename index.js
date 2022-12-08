111e67e246f65bfe304316c6624c53482b4a3b63
Seven Chord <hjlcarl@163.com>
2022-12-08 11:12:10 +0800

    build: :package: 换用pnpm

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { appMain } from "./app/index.js";
 import { moduleMain } from "./script/index.js";
 import { pluginMain } from "./plugin/index.js";
-import { updateStyle, root } from "./util/resource.js";
 
 export function main() {
   appMain();

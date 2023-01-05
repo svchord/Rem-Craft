789d8b75e1c7ceeacfa74f5bb0c7d76709a0fb04
Seven Chord <hjlcarl@163.com>
2023-01-05 11:32:47 +0800

    fix: :bug: 增加移动端判断
    link #98

--- theme.js
+++ theme.js
@@ -1,7 +1,12 @@
-import("./index.js")
-  .then((index) => {
-    index.main();
-  })
-  .catch((e) => {
-    console.error(e);
-  });
+if (
+  window.siyuan.config.system.container !== "ios" &&
+  window.siyuan.config.system.container !== "android"
+) {
+  import("./index.js")
+    .then((index) => {
+      index.main();
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+}

6909c7987e128b066614e47691ebcfb13811bbbf
Seven Chord <hjlcarl@163.com>
2023-03-21 17:13:20 +0800

    fix: :bug: 优化日历样式
    link #120

--- app/calendar/main.js
+++ app/calendar/main.js
@@ -1,20 +1,20 @@
 function insertBtn(elementId) {
     // 把日历图标 放到 {{elementId}} 图标前面
-    const element = document.getElementById(elementId);
+    const element = document.getElementById(elementId)
     if (element) {
         element.insertAdjacentHTML(
             'beforebegin',
             `<div id="calendar" class="toolbar__item b3-tooltips b3-tooltips__se" aria-label="日历" ></div>`
-        );
+        )
     }
 }
 
 // 计算页面的实际高度，iframe自适应会用到
 function calcPageHeight(doc) {
-    var cHeight = Math.max(doc.body.clientHeight, doc.documentElement.clientHeight);
-    var sHeight = Math.max(doc.body.scrollHeight, doc.documentElement.scrollHeight);
-    var height = Math.max(cHeight, sHeight) + 15;
-    return height + 'px';
+    var cHeight = Math.max(doc.body.clientHeight, doc.documentElement.clientHeight)
+    var sHeight = Math.max(doc.body.scrollHeight, doc.documentElement.scrollHeight)
+    var height = Math.max(cHeight, sHeight) + 15
+    return height + 'px'
 }
 
 function initCalendar() {
@@ -36,37 +36,36 @@ function initCalendar() {
             src="/appearance/themes/Rem Craft/app/calendar/dist"
         />
         `
-    );
-    const ifm = document.getElementById('calendarIframe');
+    )
+    const ifm = document.getElementById('calendarIframe')
     if ('darwin' === window.siyuan.config.system.os) {
-        insertBtn('barSearch');
-        ifm.style.right = '50px';
+        insertBtn('barSearch')
+        ifm.style.right = '50px'
     } else {
-        insertBtn('barBack');
-        ifm.style.left = '50px';
+        insertBtn('barBack')
+        ifm.style.left = '50px'
     }
 
     ifm.onload = function () {
-        let iDoc = ifm.contentDocument || ifm.document;
-        ifm.style.height = calcPageHeight(iDoc);
-    };
+        let iDoc = ifm.contentDocument || ifm.document
+        ifm.style.height = calcPageHeight(iDoc)
+    }
 
-    const btn = document.getElementById('calendar');
+    const btn = document.getElementById('calendar')
     if (btn) {
-        btn.innerHTML = `<svg t="1662957805816" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2374" width="200" height="200"><path d="M797.257 402.286h-570.514v113.371h570.514v-113.371zM910.629 76.8h-58.514v-76.8h-113.371v76.8h-453.486v-76.8h-113.371v76.8h-58.514c-62.171 0-113.371 51.2-113.371 113.371v724.114c0 62.171 51.2 109.714 113.371 109.714h797.257c62.171 0 113.371-47.543 113.371-109.714v-724.114c0-62.171-51.2-113.371-113.371-113.371zM910.629 914.286h-797.257v-625.371h797.257v625.371zM625.371 629.029h-398.629v113.371h398.629v-113.371z"></path></svg>`;
-
+        btn.innerHTML = '<svg><use xlink:href="#iconCalendar"></use></svg>'
         btn.addEventListener(
             'click',
             function (e) {
-                e.stopPropagation();
+                e.stopPropagation()
                 if (ifm.style.visibility === 'hidden') {
-                    ifm.style.visibility = 'visible';
+                    ifm.style.visibility = 'visible'
                 } else {
-                    ifm.style.visibility = 'hidden';
+                    ifm.style.visibility = 'hidden'
                 }
             },
             false
-        );
+        )
     }
 
     // 点击其他区域时，隐藏日历面板
@@ -74,17 +73,17 @@ function initCalendar() {
         'click',
         () => {
             if (ifm.style.visibility === 'visible') {
-                ifm.style.visibility = 'hidden';
+                ifm.style.visibility = 'hidden'
             }
         },
         false
-    );
+    )
 }
 
 export function calendarMain() {
     try {
-        initCalendar();
+        initCalendar()
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
 }
